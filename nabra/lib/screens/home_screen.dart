8fbe9f295f39e041f8a6b3a9144b323bd0a45db7
AhmadWaleedAlmustafa <ahamdalmostafa0099@gmail.com>
2026-02-17 15:51:21 +0300

    no thing

--- nabra/lib/screens/home_screen.dart
+++ nabra/lib/screens/home_screen.dart
@@ -263,7 +263,7 @@ class _HomeScreen extends State<HomeScreen> {
                 ),
 
                 Padding(
-                  padding: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(10),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(10),
                     child: InkWell(
