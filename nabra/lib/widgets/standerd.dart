326c4198dcc631c529a44f7030a18fe251818940
AhmadWaleedAlmustafa <ahamdalmostafa0099@gmail.com>
2026-03-31 02:15:48 +0300

    changed drawer icon color

--- nabra/lib/widgets/standerd.dart
+++ nabra/lib/widgets/standerd.dart
@@ -11,6 +11,7 @@ class Standerd extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
         flexibleSpace: Image.asset(
           'assets/images/image_5.png',
           fit: BoxFit.cover,
